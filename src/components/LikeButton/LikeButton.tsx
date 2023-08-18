80ffcc341c340852f26c084005a1ca85a163119a
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-08-18 13:09:33 +0300

    ADD: teacher avatar, teacher basic info, like button

---
+++ src/components/LikeButton/LikeButton.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import s from "./LikeButton.module.scss";
+
+const LikeButton = ({
+  handleClick,
+  id,
+  icon,
+  alt,
+}: {
+  handleClick?: Function;
+  id?: number;
+  icon?: string;
+  alt?: string;
+}) => {
+  return (
+    <button
+      type="button"
+      // onClick={() => handleClick(id)}
+      className={s.liked}
+    >
+      <img src={icon} alt={alt} />
+    </button>
+  );
+};
+
+LikeButton.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+  icon: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
+export default LikeButton;
--- src/components/TeachersList/TeachersList.tsx
+++ src/components/TeachersList/TeachersList.tsx
@@ -1,13 +1,51 @@
 import React from "react";
 import s from "./TeachersList.module.scss";
 import { teachers } from "../../services/teachers";
+import LikeButton from "../LikeButton/LikeButton";
+import Like from "../../assets/icons/like.svg";
+import Liked from "../../assets/icons/liked.svg";
 
 const TeachersList = () => {
   return (
-    <ul>
+    <ul className={s.teachersList}>
       {teachers.map((teacher, index) => (
         <li key={index} className={s.teacherCard}>
-          <span>{teacher.name}</span> <span>{teacher.surname}</span>
+          <div>
+            <div className={s.roundImageBorder}>
+              <img
+                className={s.teacherPhoto}
+                src={teacher.avatar_url}
+                alt="teacher"
+              />
+            </div>
+          </div>
+          <div>
+            <LikeButton icon={Like} alt={"like"} />
+            <ul className={s.teacherCommonInfo}>
+              <li>
+                <span className={s.languages}>Languages</span>
+              </li>
+              <li>
+                <span className={s.lessonsOnline}>Lessons online</span>
+              </li>
+              <li>
+                <span className={s.lessonsDone}>
+                  Lessons done: {teacher.lessons_done}
+                </span>
+              </li>
+              <li>
+                <span className={s.rating}>Rating: {teacher.rating}</span>
+              </li>
+              <li>
+                <span className={s.price}>
+                  Price / 1 hour:{" "}
+                  <span className={s.priceHighlight}>
+                    {teacher.price_per_hour}$
+                  </span>
+                </span>
+              </li>
+            </ul>
+          </div>
         </li>
       ))}
     </ul>
